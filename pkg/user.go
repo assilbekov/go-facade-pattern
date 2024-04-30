ead506d0636cf5d2d7d727980e5da2ee1c85cf64
assilbekov <askhat.assilbekov@gmail.com>
2024-04-30 14:35:52 -0300

    add user balance and pay

--- pkg/user.go
+++ pkg/user.go
@@ -4,3 +4,11 @@ type User struct {
 	Name string
 	Card *Card
 }
+
+func (u *User) Balance() float64 {
+	return u.Card.Balance
+}
+
+func (u *User) Pay(amount float64) {
+	u.Card.Balance -= amount
+}
